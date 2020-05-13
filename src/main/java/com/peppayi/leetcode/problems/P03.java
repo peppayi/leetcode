bae4ce61b563996cb784ccb22730978b9d83c9af
xmucoder <xmucoder@gmail.com>
2020-05-13 22:07:53 +0800

    P03 - findRepeatNumber - swap num to its final place to get O(n) solution

--- src/main/java/com/peppayi/leetcode/problems/P03.java
+++ src/main/java/com/peppayi/leetcode/problems/P03.java
@@ -5,15 +5,30 @@ public class P03 {
     public int findRepeatNumber(int[] nums) {
         assert nums.length >= 2 && nums.length <= 100000;
 
-        boolean[] exists = new boolean[nums.length];
-        for (int n : nums) {
-            if (exists[n]) {
-                return n;
-            }
+        int N = nums.length;
 
-            exists[n] = true;
+        int i = 0;
+        while (i < N) {
+            int v = nums[i];
+            assert v >= 0 && v < N;
+
+            if (v == i) {
+                i++;
+            }
+            else if (v == nums[v]) {
+                return v;
+            }
+            else {
+                swap(nums, i, v);
+            }
         }
 
-        return -1;
+        throw new IllegalArgumentException("no duplicate numbers found");
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
     }
 }
