766f4c224697790582c0d1956c7caea78cde8022
xmucoder <xmucoder@gmail.com>
2020-05-14 22:39:19 +0800

    P06 - reversePrint - two pass approach

---
+++ src/main/java/com/peppayi/leetcode/problems/P06.java
@@ -0,0 +1,27 @@
+package com.peppayi.leetcode.problems;
+
+import com.peppayi.leetcode.ListNode;
+
+public class P06 {
+
+    public int[] reversePrint(ListNode head) {
+        if (head == null) return new int[0];
+
+        int size = 0;
+        ListNode ptr = head;
+        while (ptr != null) {
+            size++;
+            ptr = ptr.next;
+        }
+
+        int[] rv = new int[size];
+        int i = size - 1;
+        ptr = head;
+        while (ptr != null) {
+            rv[i--] = ptr.val;
+            ptr = ptr.next;
+        }
+
+        return rv;
+    }
+}
