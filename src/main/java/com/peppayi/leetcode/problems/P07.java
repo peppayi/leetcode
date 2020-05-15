e86eecd090aa1d154ec8d060dea093857c122184
xmucoder <xmucoder@gmail.com>
2020-05-15 21:30:38 +0800

    P07 - buildTree - recursive approach

---
+++ src/main/java/com/peppayi/leetcode/problems/P07.java
@@ -0,0 +1,28 @@
+package com.peppayi.leetcode.problems;
+
+public class P07 {
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (preorder == null || preorder.length == 0) return null;
+
+        assert preorder.length == inorder.length;
+        return buildTree(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1);
+    }
+
+    private TreeNode buildTree(int[] preorder, int pi, int pe, int[] inorder, int ii, int ie) {
+        if (pi > pe || ii > ie) return null;
+        else if (pi == pe) return new TreeNode(preorder[pi]);
+
+        int rootValue = preorder[pi];
+        int leftCount = 0;
+        while (ii + leftCount <= ie && inorder[ii + leftCount] != rootValue) {
+            leftCount++;
+        }
+
+        TreeNode root = new TreeNode(rootValue);
+        root.left = buildTree(preorder, pi + 1, pi + leftCount, inorder, ii, ii + leftCount - 1);
+        root.right = buildTree(preorder, pi + leftCount + 1, pe, inorder, ii + leftCount + 1, pe);
+
+        return root;
+    }
+}
---
+++ src/main/java/com/peppayi/leetcode/problems/TreeNode.java
@@ -0,0 +1,14 @@
+package com.peppayi.leetcode.problems;
+
+public class TreeNode {
+
+    public int val;
+
+    public TreeNode left;
+
+    public TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
