8a037a7bd53c3caae26f1db4a90cc00e2dcc77ce
xmucoder <xmucoder@gmail.com>
2020-05-15 21:38:03 +0800

    P08 - CQueue - normal approach

---
+++ src/main/java/com/peppayi/leetcode/problems/CQueue.java
@@ -0,0 +1,35 @@
+package com.peppayi.leetcode.problems;
+
+import java.util.Stack;
+
+public class CQueue {
+
+    Stack<Integer> in;
+
+    Stack<Integer> out;
+
+    public CQueue() {
+        this.in = new Stack<>();
+        this.out = new Stack<>();
+    }
+
+    public void appendTail(int value) {
+        in.push(value);
+    }
+
+    public int deleteHead() {
+        if (out.isEmpty()) {
+            while (! in.isEmpty()) {
+                int v = in.pop();
+                out.push(v);
+            }
+        }
+
+        if (out.isEmpty()) {
+            return -1;
+        }
+        else {
+            return out.pop();
+        }
+    }
+}
