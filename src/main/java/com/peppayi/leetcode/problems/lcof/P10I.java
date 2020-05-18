1c4ffb1d5b81294d22cda22ec64ad5950036fad9
xmucoder <xmucoder@gmail.com>
2020-05-18 22:34:38 +0800

    P10-I - fib - dp approach

--- src/main/java/com/peppayi/leetcode/problems/lcof/P10I.java
+++ src/main/java/com/peppayi/leetcode/problems/lcof/P10I.java
@@ -3,17 +3,15 @@ package com.peppayi.leetcode.problems.lcof;
 public class P10I {
 
     public int fib(int n) {
-        int a = 0, b = 1;
+        if (n == 0) return 0;
+        if (n == 1) return 1;
 
-        if (n == 0) return a;
-        if (n == 1) return b;
-
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
         for (int i = 2; i <= n; ++i) {
-            int c = (a + b) % 1000000007;
-            a = b;
-            b = c;
+            dp[i] = (dp[i-1] + dp[i-2]) % 1000000007;
         }
-
-        return b;
+        return dp[n];
     }
 }
