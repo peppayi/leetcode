56feb35e9fd7c7cfffdbf0e424ee46c196e7af28
xmucoder <xmucoder@gmail.com>
2020-05-18 22:17:44 +0800

    P10-I - fib - recursive approach

---
+++ src/main/java/com/peppayi/leetcode/problems/lcof/P10I.java
@@ -0,0 +1,10 @@
+package com.peppayi.leetcode.problems.lcof;
+
+public class P10I {
+
+    public int fib(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        return fib(n - 1) + fib(n - 2);
+    }
+}
