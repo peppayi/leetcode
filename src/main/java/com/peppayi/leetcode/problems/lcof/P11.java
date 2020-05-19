c54294527497e957029b2d5a0e51c2783bb35aaf
xmucoder <xmucoder@gmail.com>
2020-05-19 22:02:11 +0800

    P11 - minArray - binary search

--- src/main/java/com/peppayi/leetcode/problems/lcof/P11.java
+++ src/main/java/com/peppayi/leetcode/problems/lcof/P11.java
@@ -5,15 +5,22 @@ public class P11 {
     public int minArray(int[] numbers) {
         if (numbers == null || numbers.length == 0) throw new IllegalArgumentException("numbers is empty");
 
-        int i = 0;
-        while (i < numbers.length - 1) {
-            if (numbers[i] > numbers[i + 1]) {
-                return numbers[i + 1];
-            }
+        int N = numbers.length;
+        int lo = 0, hi = N - 1;
+        while (lo < hi) {
+            int mi = (hi - lo) / 2 + lo;
 
-            i++;
+            if (numbers[mi] > numbers[hi]) {
+                lo = mi + 1;
+            }
+            else if (numbers[mi] < numbers[lo]){
+                hi = mi - 1;
+            }
+            else {
+                hi--;
+            }
         }
 
-        return numbers[0];
+        return numbers[lo];
     }
 }
