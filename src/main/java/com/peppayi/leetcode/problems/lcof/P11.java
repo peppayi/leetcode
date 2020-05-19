1463222248b0ce653bf145c3fe10cc49daf917c0
xmucoder <xmucoder@gmail.com>
2020-05-19 22:06:18 +0800

    P11 - minArray - recursive approach

--- src/main/java/com/peppayi/leetcode/problems/lcof/P11.java
+++ src/main/java/com/peppayi/leetcode/problems/lcof/P11.java
@@ -5,22 +5,15 @@ public class P11 {
     public int minArray(int[] numbers) {
         if (numbers == null || numbers.length == 0) throw new IllegalArgumentException("numbers is empty");
 
-        int N = numbers.length;
-        int lo = 0, hi = N - 1;
-        while (lo < hi) {
-            int mi = (hi - lo) / 2 + lo;
+        return minArray(numbers, 0, numbers.length - 1);
+    }
 
-            if (numbers[mi] > numbers[hi]) {
-                lo = mi + 1;
-            }
-            else if (numbers[mi] < numbers[lo]){
-                hi = mi - 1;
-            }
-            else {
-                hi--;
-            }
+    private int minArray(int[] numbers, int start, int end) {
+        if (start == end) return numbers[start];
+        else if (start + 1 == end) return Math.min(numbers[start], numbers[end]);
+        else {
+            int mid = (end - start) / 2 + start;
+            return Math.min(minArray(numbers, start, mid), minArray(numbers, mid + 1, end));
         }
-
-        return numbers[lo];
     }
 }
