00bce80232ab1f837ca841b0f2cc4a214ca26d31
xmucoder <xmucoder@gmail.com>
2020-05-21 21:57:10 +0800

    P13 - movingCount - BFS approach

--- src/main/java/com/peppayi/leetcode/problems/lcof/P13.java
+++ src/main/java/com/peppayi/leetcode/problems/lcof/P13.java
@@ -1,16 +1,27 @@
 package com.peppayi.leetcode.problems.lcof;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 public class P13 {
 
     public int movingCount(int m, int n, int k) {
-        return movingCount(m, n, k, 0, 0, new boolean[m][n]);
-    }
+        boolean[][] visited = new boolean[m][n];
+        int count = 0;
 
-    private int movingCount(int m, int n, int k, int mi, int ni, boolean[][] visited) {
-        if (mi == m || ni == n || visited[mi][ni] || indexSum(mi) + indexSum(ni) > k) return 0;
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.add(new int[] {0, 0});
+        while (! queue.isEmpty()) {
+            int[] e = queue.poll();
+            int mi = e[0], ni = e[1];
+            if (mi == m || ni == n || visited[mi][ni] || indexSum(mi) + indexSum(ni) > k) continue;
+            visited[mi][ni] = true;
+            count++;
+            queue.add(new int[]{mi + 1, ni});
+            queue.add(new int[]{mi, ni + 1});
+        }
 
-        visited[mi][ni] = true;
-        return 1 + movingCount(m, n, k, mi + 1, ni, visited) + movingCount(m, n, k, mi, ni + 1, visited);
+        return count;
     }
 
     private int indexSum(int i) {
