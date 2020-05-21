3e5481c79bb59705724197130546ba8d634a4e61
xmucoder <xmucoder@gmail.com>
2020-05-21 21:46:59 +0800

    P13 - movingCount - recursive approach improved

--- src/main/java/com/peppayi/leetcode/problems/lcof/P13.java
+++ src/main/java/com/peppayi/leetcode/problems/lcof/P13.java
@@ -7,18 +7,10 @@ public class P13 {
     }
 
     private int movingCount(int m, int n, int k, int mi, int ni, boolean[][] visited) {
-        if (mi < 0 || mi == m || ni < 0 || ni == n) return 0;
-        if (visited[mi][ni]) return 0;
+        if (mi == m || ni == n || visited[mi][ni] || indexSum(mi) + indexSum(ni) > k) return 0;
 
-        int sum = indexSum(mi) + indexSum(ni);
-        if (sum <= k) {
-            visited[mi][ni] = true;
-            return 1 + movingCount(m, n, k, mi - 1, ni, visited) + movingCount(m, n, k, mi + 1, ni, visited) +
-                    movingCount(m, n, k, mi, ni - 1, visited) + movingCount(m, n, k, mi, ni + 1, visited);
-        }
-        else {
-            return 0;
-        }
+        visited[mi][ni] = true;
+        return 1 + movingCount(m, n, k, mi + 1, ni, visited) + movingCount(m, n, k, mi, ni + 1, visited);
     }
 
     private int indexSum(int i) {
