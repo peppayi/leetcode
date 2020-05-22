7db2014a3138377e0f61c95305f859903d839bd3
xmucoder <xmucoder@gmail.com>
2020-05-22 22:52:24 +0800

    P14 - cuttingRope - greed approach

---
+++ src/main/java/com/peppayi/leetcode/problems/lcof/P14.java
@@ -0,0 +1,20 @@
+package com.peppayi.leetcode.problems.lcof;
+
+public class P14 {
+
+    public int cuttingRope(int n) {
+        assert n >= 2 && n <= 58;
+
+        if (n <= 3) return n - 1;
+
+        int multiply = 1;
+        while (n - 3 > 1) {
+            multiply *= 3;
+            n -= 3;
+        }
+
+        multiply *= n;
+
+        return multiply;
+    }
+}
