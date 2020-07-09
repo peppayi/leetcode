8d7f0f46dcd971a47810332b9fc92cb59709e2f1
xmucoder <xmucoder@gmail.com>
2020-07-09 22:07:18 +0800

    P001 - with the help of hashmap to reduce time complex - O(n)

--- src/main/java/com/peppayi/leetcode/problems/bytedance/P001.java
+++ src/main/java/com/peppayi/leetcode/problems/bytedance/P001.java
@@ -1,15 +1,23 @@
 package com.peppayi.leetcode.problems.bytedance;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class P001 {
 
     public int[] twoSum(int[] nums, int target) {
         if (nums == null || nums.length == 0) throw new IllegalArgumentException("nums is empty");
 
+        Map<Integer, Integer> caches = new HashMap<>(nums.length, 1);
         for (int i = 0; i < nums.length; ++i) {
-            for (int j = i + 1; j < nums.length; ++j) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[] {i, j};
-                }
+            int n = nums[i];
+            int complement = target - n;
+            Integer index = caches.get(complement);
+            if (index == null) {
+                caches.put(n, i);
+            }
+            else {
+                return new int[] {index, i};
             }
         }
 
