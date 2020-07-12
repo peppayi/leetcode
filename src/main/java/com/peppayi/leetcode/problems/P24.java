59c65e9507f86c95669638bb02b827a502b8f930
xmucoder <xmucoder@gmail.com>
2020-07-12 20:27:31 +0800

    P24 - iterate approach - O(n)

--- src/main/java/com/peppayi/leetcode/problems/P24.java
+++ src/main/java/com/peppayi/leetcode/problems/P24.java
@@ -7,9 +7,23 @@ public class P24 {
     public ListNode swapPairs(ListNode head) {
         if (head == null || head.next == null) return head;
 
-        ListNode first = head, second = head.next;
-        first.next = swapPairs(second.next);
-        second.next = first;
-        return second;
+        ListNode sentinel = new ListNode(0);
+        sentinel.next = head;
+
+        ListNode prev = sentinel;
+        ListNode curr = head;
+        while (curr != null && curr.next != null) {
+            ListNode second = curr.next;
+            ListNode next = second.next;
+
+            prev.next = second;
+
+            second.next = curr;
+            curr.next = next;
+            prev = curr;
+            curr = next;
+        }
+
+        return sentinel.next;
     }
 }
